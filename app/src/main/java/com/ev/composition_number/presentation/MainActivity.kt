25ca70b1908cbb39396ac12a94803a6d8416b381
dmitriyy.ev@gmail.com <SpD34$otf$>
2022-06-21 17:53:19 +0300

    макеты

--- app/src/main/java/com/ev/composition_number/presentation/MainActivity.kt
+++ app/src/main/java/com/ev/composition_number/presentation/MainActivity.kt
@@ -3,10 +3,15 @@ package com.ev.composition_number.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ev.composition_number.R
+import com.ev.composition_number.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
