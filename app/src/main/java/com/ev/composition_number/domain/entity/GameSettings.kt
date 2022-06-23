7a10671f512eae8472a261cba2290b2f18fd173b
dmitriyy.ev@gmail.com <SpD34$otf$>
2022-06-23 15:12:03 +0300

    Parcelable

--- app/src/main/java/com/ev/composition_number/domain/entity/GameSettings.kt
+++ app/src/main/java/com/ev/composition_number/domain/entity/GameSettings.kt
@@ -1,10 +1,13 @@
 package com.ev.composition_number.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
+@Parcelize
 data class GameSettings(
     val maxSum: Int,
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val gameTime: Int
-): Serializable
+): Parcelable
